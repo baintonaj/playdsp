b678e15a8f4c4b2e0271de84ac94c4ef046cd8c3
Andy Bainton <60830258+baintonaj@users.noreply.github.com>
2024-11-01 21:48:40 -0700

    Complete the merge to main.

--- audio/processing/rust_process_audio.rs
+++
@@ -1,9 +0,0 @@
-pub fn rust_process(input: &Vec<Vec<f64>>, output: &mut Vec<Vec<f64>>) {
-    let gain_raw = 10.0_f64.powf(-6.0 / 20.0);
-
-    for (in_channel, out_channel) in input.iter().zip(output.iter_mut()) {
-        for (in_sample, out_sample) in in_channel.iter().zip(out_channel.iter_mut()) {
-            *out_sample = in_sample * gain_raw;
-        }
-    }
-}
